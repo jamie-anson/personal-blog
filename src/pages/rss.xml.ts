b92e8073d1fc22021a008ee446bd59470e04ccc9
Jamie Anson <22300767+jamie-anson@users.noreply.github.com>
2026-04-03 00:26:38 +0100

    Add RSS feed support

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,32 +1,47 @@
 import { getCollection } from 'astro:content';
 import { site } from '../lib/site';
 
+function xmlEscape(value: string) {
+  return value
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&apos;');
+}
+
 export async function GET() {
   const posts = (await getCollection('posts', ({ data }) => !data.draft)).sort(
     (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
   );
 
+  const feedUrl = `${site.url}/rss.xml`;
   const items = posts
     .map((post) => {
       const url = `${site.url}/posts/${post.slug}/`;
+      const description = xmlEscape(post.data.description);
+      const title = xmlEscape(post.data.title);
 
       return `
         <item>
-          <title><![CDATA[${post.data.title}]]></title>
+          <title>${title}</title>
           <link>${url}</link>
-          <guid>${url}</guid>
+          <guid isPermaLink="true">${url}</guid>
           <pubDate>${post.data.pubDate.toUTCString()}</pubDate>
-          <description><![CDATA[${post.data.description}]]></description>
+          <description>${description}</description>
         </item>`;
     })
     .join('');
 
   const xml = `<?xml version="1.0" encoding="UTF-8" ?>
-    <rss version="2.0">
+    <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
       <channel>
-        <title>${site.title}</title>
-        <description>${site.description}</description>
+        <title>${xmlEscape(site.title)}</title>
+        <description>${xmlEscape(site.description)}</description>
         <link>${site.url}</link>
+        <language>en-gb</language>
+        <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
+        <atom:link href="${feedUrl}" rel="self" type="application/rss+xml" />
         ${items}
       </channel>
     </rss>`;
